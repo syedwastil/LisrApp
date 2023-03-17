a06ba68c7246e603d4474a9f133762e81d0423dc
Muhammad Abdur Rehman Wasti <45505896+syedwastil@users.noreply.github.com>
2023-03-18 02:12:48 +0500

    state

---
+++ src/state/index.js
@@ -0,0 +1,49 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState={
+Lists:[],
+update:false,
+updateList:{},
+}
+
+export const listSlice=createSlice({
+    name:"list",
+    initialState,
+    reducers:{
+        createList:(state,action)=>{
+            let List=action.payload.List
+            state.Lists.unshift(List)
+        },
+        updateList:(state,action)=>{
+            const {title,description}=action.payload.List
+            const updatedLists=state.Lists.map((List)=>{
+                if(List!=undefined && List.id===action.payload.List.id){
+                    return {...List,title:title,description:description}
+                }else{
+                    return List
+                }
+                
+            });state.Lists=updatedLists
+        },
+        deleteList:(state,action)=>{
+    
+            const updatedLists=state.Lists.map((List)=>{
+                if(List!=undefined){
+                    if(List.id!=action.payload.List.id){
+                        return List
+                    }
+                }
+            })
+            state.Lists=updatedLists
+            console.log(state.Lists)
+        },
+        setUpdate:(state,action)=>{
+            state.update=action.payload.update;
+            state.updateList=action.payload.updateList;
+        }
+
+    }
+})
+
+export const {createList,updateList,deleteList,setUpdate}=listSlice.actions;
+export default listSlice.reducer;
